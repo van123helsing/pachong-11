9a58009e86cb2857c58546bd4d1c0ba9c7dbd002
hououin <33634174+hououin@users.noreply.github.com>
2020-05-01 11:51:45 +0200

    popravi content

--- implementation-extraction/regular_expression.py
+++ implementation-extraction/regular_expression.py
@@ -1,6 +1,6 @@
 import json
 import re
-from lxml import html
+import regex
 from pathlib import Path
 
 
@@ -15,16 +15,24 @@ def convertArray(a):
 def process_regex(page_content, type):
 
     if type == 'rtvslo':
-        a = re.compile(r"<p class=\"Body\">([\s\S]*?)<div class=\"gallery\">").search(page_content).group(1),
-        b = "".join(a)
-        c = re.sub('<[^<]+?>', '', b).replace('\n', '').strip()
+
+        content_matches = regex.compile(r"<article class=\"article\">[\s\S]*?(<p[\s\S]*?>([\s\S]*?)<\/p[\s\S]*?>[\s\S]*?)*<\/article>").finditer(page_content)
+        content = ""
+        #print(content_matches)
+        for i in content_matches:
+            for capture in i.captures(2):
+                #print(capture)
+                content += capture
+
+        #print(content)
+
         data = {
             "author" : re.compile( r"<div class=\"author-name\">([a-zA-Z]+\s+[a-zA-Z]+)").search(page_content).group(1),
             "publishedTime" : re.compile( r"<div class=\"publish-meta\">\s+([0-9]*.\s+[a-z]+\s+[0-9]*\s+[a-z]+\s+[0-9]+\:[0-9]+)").search(page_content).group(1),
             "title" :  re.compile( r"<header class=\"article-header\">[\s\S]+?<h1>([\s\S]+?)<\/h1>[\s\S]+?<\/header>").search(page_content).group(1),
             "subTitle" : re.compile( r"<header class=\"article-header\">[\s\S]+?<div class=\"subtitle\">([\s\S]+?)<\/div>[\s\S]+?<\/header>").search(page_content).group(1),
             "lead" : re.compile( r"<header class=\"article-header\">[\s\S]+?<p class=\"lead\">([\s\S]+?)<\/p>[\s\S]+?<\/header>").search(page_content).group(1),
-            "content" : c#re.compile( r"<div class=\"article-body\">[\s\S]+?<article class=\"article\">([\s\S]+?)<\/article>[\s\S]+?<\/div>").search(page_content).group(1),
+            "content" : content
         }
     elif type == 'overstock':
         data = {}
@@ -57,10 +65,19 @@ def process_regex(page_content, type):
     elif type == 'rac_novice':
 
 
-        a1 = re.compile(r"<div class=.*? id=\"single-art-text\">([\s\S]*?)<div class=\"mt10\" id=\"fb-root\">").search(page_content).group()
-        b1 = "".join(a1)
-        c1 = re.sub('<[^<]+?>', '', b1).replace('\n', '').strip()
+        # a1 = re.compile(r"<div class=.*? id=\"single-art-text\">([\s\S]*?)<div class=\"mt10\" id=\"fb-root\">").search(page_content).group()
+        # b1 = "".join(a1)
+        # c1 = re.sub('<[^<]+?>', '', b1).replace('\n', '').strip()
+        # print(c1)
 
+        content_match = regex.compile(r"<div .*? id=\"single-art-text\">[\s\S]*?(<p[\s\S]*?>[\s]*?(<.*?>)?([\s\S]*?)[\s]*?(<.*?>)?[\s]*?<\/p[\s\S]*?>[\s\S]*?)*<div .*? id=\"fb-root\">").finditer(page_content)
+        content = ""
+        for z in content_match:
+            for capture in z.captures(3):
+                #print(capture)
+                content += capture
+
+        #print(content)
 
         data = {
             "category" : re.compile( r"<div id=\"whole-path\" class=\"intextAdIgnore\">[\S\s]*?<a href=.*?>(.*?)<\/a>[\S\s]*?<a href=.*?>(.*?)<\/a>").search(page_content).group(1) + " -> " + re.compile( r"<div id=\"whole-path\" class=\"intextAdIgnore\">[\S\s]*?<a href=.*?>(.*?)<\/a>[\S\s]*?<a href=.*?>(.*?)<\/a>").search(page_content).group(2),
@@ -68,7 +85,7 @@ def process_regex(page_content, type):
             "publishedTime" :  re.compile( r"<div class=\"single-art-date fl mr10\">([\s\S]*?)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;([\s\S]*?)<\/div>").search(page_content).group(1)+ " " +re.compile( r"<div class=\"single-art-date fl mr10\">([\s\S]*?)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;([\s\S]*?)<\/div>").search(page_content).group(2),
             "title" : re.compile( r"<h1 class=\"h1 news-title-redesign intextAdIgnore\">(.*?)<\/h1>").search(page_content).group(1),
             "subTitle" : convertArray(re.compile( r"<p><strong>(.*?)<\/strong><\/p>").findall(page_content)),
-            "content" : c1,
+            "content" : content
         }
 
     elif type == 'ceneje':
@@ -99,40 +116,41 @@ def saveToFile(fileName, data):
 
 def process():
 
-    # rtvslo1 = open('Webpages/Audi.html','r', encoding='utf8').read()
+    rtvslo1 = open('Webpages/Audi.html','r', encoding='utf8').read()
     # with open("text.txt", "w", encoding="utf8") as f:
     #     f.write(rtvslo1)
-    # rtvslo1_data = process_regex(rtvslo1,"rtvslo")
-    # saveToFile("Re-rtv1-output.json", rtvslo1_data)
-    #
-    # rtvslo2 = open('Webpages/Volvo.html','r', encoding='utf8').read()
-    # rtvslo2_data = process_regex(rtvslo2,"rtvslo")
-    # saveToFile("Re-rtv2-output.json", rtvslo2_data)
+    rtvslo1_data = process_regex(rtvslo1,"rtvslo")
+    saveToFile("Re-rtv1-output.json", rtvslo1_data)
 
-    # rac_novice1 = open('Webpages/RacNovice1.html','r', encoding='utf8').read()
+    rtvslo2 = open('Webpages/Volvo.html','r', encoding='utf8').read()
+    rtvslo2_data = process_regex(rtvslo2,"rtvslo")
+    saveToFile("Re-rtv2-output.json", rtvslo2_data)
+
+    rac_novice1 = open('Webpages/RacNovice1.html','r', encoding='utf8').read()
     # with open("text.txt", "w", encoding="utf8") as f:
     #     f.write(rac_novice1)
-    # rac_novice1_data = process_regex(rac_novice1,"rac_novice")
-    # saveToFile("Re-racNovice-output.json", rac_novice1_data)
+    rac_novice1_data = process_regex(rac_novice1,"rac_novice")
+    saveToFile("Re-racNovice1-output.json", rac_novice1_data)
 
-    # rac_novice2 = open('Webpages/RacNovice2.html','r', encoding='utf8').read()
-    # rac_novice2_data = process_regex(rac_novice2,"rac_novice")
-    # saveToFile("Re-racNovice-output.json", rac_novice2_data)
+    rac_novice2 = open('Webpages/RacNovice2.html','r', encoding='utf8').read()
+    rac_novice2_data = process_regex(rac_novice2,"rac_novice")
+    saveToFile("Re-racNovice2-output.json", rac_novice2_data)
 
-    # overstock1 = open('WebPages/jewelry01.html','r', encoding='utf-8', errors='ignore').read()
-    # doverstock1_data = process_regex(overstock1, "overstock")
-    # saveToFile("Re-overstock1-output.json", doverstock1_data)
-    #
-    # overstock2 = open('WebPages/jewelry02.html','r', encoding='utf-8', errors='ignore').read()
-    # doverstock2_data = process_regex(overstock2, "overstock")
-    # saveToFile("Re-overstock2-output.json", doverstock2_data)
+    overstock1 = open('WebPages/jewelry01.html','r', encoding='utf-8', errors='ignore').read()
+    doverstock1_data = process_regex(overstock1, "overstock")
+    saveToFile("Re-overstock1-output.json", doverstock1_data)
+
+    overstock2 = open('WebPages/jewelry02.html','r', encoding='utf-8', errors='ignore').read()
+    doverstock2_data = process_regex(overstock2, "overstock")
+    saveToFile("Re-overstock2-output.json", doverstock2_data)
 
     ceneje1 = open('WebPages/PC-Ceneje.si.html', 'r', encoding='utf-8', errors='ignore').read()
-    # with open("text.txt", "w", encoding="utf8") as f:
-    #     f.write(ceneje1)
     ceneje1_data = process_regex(ceneje1, "ceneje")
-    saveToFile("Re-ceneje-output.json", ceneje1_data)
+    saveToFile("Re-ceneje1-output.json", ceneje1_data)
 
+    ceneje2 = open('WebPages/Kavci-Ceneje.si.html', 'r', encoding='utf-8', errors='ignore').read()
+    ceneje2_data = process_regex(ceneje2, "ceneje")
+    saveToFile("Re-ceneje2-output.json", ceneje2_data)
 
 
 
